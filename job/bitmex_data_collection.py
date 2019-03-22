3bf0ac3a8310b9b6664d43813d09411b0b76f23a
xiangzz159 <xiangzz159@qq.com>
2019-03-22 17:32:18 +0800

    async fetch bitmex ohlcv data

--- job/bitmex_data_collection.py
+++ job/bitmex_data_collection.py
@@ -1,4 +1,4 @@
-#！/usr/bin/env python
+# ！/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
 '''
@@ -13,15 +13,40 @@
 
 '''
 
-import ccxt
-import time
-import pandas as pd
+import ccxt.async_support as ccxt
+import asyncio
+
+
+async def get_kline(symbol, periods_str, since, limit):
+    ex = ccxt.bitmex()
+    try:
+        kline = await ex.fetch_ohlcv(symbol, periods_str, since, limit)
+        await ex.close()
+        return kline
+    except ccxt.BaseError as e:
+        print(type(e).__name__, str(e), str(e.args))
+        raise e
+
 
 limit = 750
-ex = ccxt.bitmex()
-since = int(time.time()) * 1000 - 3600000 * (limit - 1)
-k = ex.fetch_ohlcv('BTC/USD', '1h', since, limit)
-df = pd.DataFrame(k, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
-df['Timestamp'] = df['Timestamp'] / 1000
-fileName = '../data/bitmex.csv'
-df.to_csv(fileName, index=None)
+symbol = 'BTC/USD'
+periods = 60 * 1000
+begin = 1514736000000
+end = begin + limit * periods * 3
+periods_str = '1m'
+async_funcs = []
+tt = end - begin
+count = int(tt / periods / limit)
+begin_ = begin
+for i in range(count):
+    async_funcs.append(get_kline(symbol, periods_str, begin_, limit))
+    begin_ += periods * limit
+loop = asyncio.get_event_loop()
+klines = loop.run_until_complete(asyncio.gather(*async_funcs))
+for kline in klines:
+    print(kline)
+
+# df = pd.DataFrame(k, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+# df['Timestamp'] = df['Timestamp'] / 1000
+# fileName = '../data/bitmex.csv'
+# df.to_csv(fileName, index=None)
