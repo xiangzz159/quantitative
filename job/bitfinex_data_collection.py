66b770f3ceaf401d790f865b07c4005ddac76f1a
xiangzz159 <xiangzz159@qq.com>
2019-04-19 10:52:57 +0800

    add bitfinex data collection

---
+++ job/bitfinex_data_collection.py
@@ -0,0 +1,50 @@
+# ！/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+'''
+
+@author: yerik
+
+@contact: xiangzz159@qq.com
+
+@time: 2019/4/19 10:26
+
+@desc: 
+
+'''
+
+import json
+import numpy as np
+import os
+import pandas as pd
+from urllib import request
+import time
+
+symbol = 'tBTCUSD'
+# Available values: '1m', '5m', '15m', '30m', '1h', '3h', '6h', '12h', '1D', '7D', '14D', '1M'
+timeframe = '1h'
+# max:5000
+limit = 5000
+# ms
+start = 1514736000000
+try:
+    url = 'https://api-pub.bitfinex.com/v2/candles/trade:%s:%s/hist?limit=%d&start=%d&sort=1' % (
+        timeframe, symbol, limit, start)
+    print('URL: %s' % url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+    req = request.Request(url=url, headers=headers, method='GET')
+    openUrl = request.urlopen(req)
+
+    r = openUrl.read()
+    d = json.loads(r.decode())
+    df = pd.DataFrame(d, columns=['Timestamp', 'Open', 'Close', 'High', 'Low', 'Volume'])
+
+    df['Timestamp'] = df['Timestamp'] / 1000
+    date = time.strftime("%Y-%m-%d", time.localtime(start / 1000))
+    fileName = '../data/Bitfinex%s-%s.csv' % (date, "now-1H")
+    df.to_csv(fileName, index=False)
+except BaseException as e:
+    print(e)
--- job/bitmex_data_collection.py
+++ job/bitmex_data_collection.py
@@ -7,46 +7,43 @@
 
 @contact: xiangzz159@qq.com
 
-@time: 2018/11/28 10:59
+@time: 2019/3/22 17:51
 
 @desc:
 
 '''
 
-import ccxt.async_support as ccxt
-import asyncio
+import ccxt
+import time
+import numpy as np
+import pandas as pd
 
-
-async def get_kline(symbol, periods_str, since, limit):
-    ex = ccxt.bitmex()
-    try:
-        kline = await ex.fetch_ohlcv(symbol, periods_str, since, limit)
-        await ex.close()
-        return kline
-    except ccxt.BaseError as e:
-        print(type(e).__name__, str(e), str(e.args))
-        raise e
-
-
-limit = 750
+limit = 100
 symbol = 'BTC/USD'
-periods = 60 * 1000
-begin = 1514736000000
-end = begin + limit * periods * 3
-periods_str = '1m'
-async_funcs = []
-tt = end - begin
-count = int(tt / periods / limit)
-begin_ = begin
+periods = 60 * 1000 * 5
+# grid 测试开始时间
+begin = 1554220800000
+end = int(time.time()) * 1000
+count = int((end - begin) / limit / periods)
+periods_str = '5m'
+ex = ccxt.bitmex()
+klines = []
 for i in range(count):
-    async_funcs.append(get_kline(symbol, periods_str, begin_, limit))
-    begin_ += periods * limit
-loop = asyncio.get_event_loop()
-klines = loop.run_until_complete(asyncio.gather(*async_funcs))
-for kline in klines:
-    print(kline)
+    try:
+        kline = ex.fetch_ohlcv(symbol, periods_str, begin, limit)
+        klines.append(kline)
+        begin += limit * periods
+        time.sleep(1)
+        if i % 100 == 0:
+            print(i, klines)
+    except Exception as e:
+        print(e)
+    finally:
+        time.sleep(2)
 
-# df = pd.DataFrame(k, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
-# df['Timestamp'] = df['Timestamp'] / 1000
-# fileName = '../data/bitmex.csv'
-# df.to_csv(fileName, index=None)
+data = np.array(klines[0])
+for i in range(1, len(klines)):
+    data = np.append(data, klines[i], axis=0)
+df = pd.DataFrame(data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+df['Timestamp'] = df['Timestamp'] / 1000
+df.to_csv('/root/bitmex_data.csv', index=False)
