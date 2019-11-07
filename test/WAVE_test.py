127e197011809151c5b2ceb449d1d3f19561e07f
Xiang <xiangguangye@sunline.cn>
2019-11-07 23:17:42 +0800

    添加策略思路

--- test/WAVE_test.py
+++ test/WAVE_test.py
@@ -10,7 +10,9 @@
 @time: 2019/8/3 18:32
 
 @desc:
-
+1. 时间周期定为7天，分别取5个波峰和波谷值作为价格上限区间和下限区间
+2. 当当前价格在上限区间时，做空；当当前价格在下限区间时，做多。
+3. 当当前时间周期最高值大于上限区间最高值或当前时间周期最小值小于下限区间最小值时，平仓并在接下来的12个小时内不再开仓
 '''
 
 from tools import data2df
@@ -22,30 +24,22 @@ from tools.stockstats import StockDataFrame
 
 
 def wave_guess(arr, wn):
+    close_list = arr[:, [0, 4]]
+
     # 计算最大的N个值，认为是波峰
-    wave_crest = heapq.nlargest(wn, enumerate(arr), key=lambda x: x[1])
+    # <class 'list'>: [(167, 4343.0), (166, 4335.0), (144, 4329.5), (146, 4329.3), (145, 4327.3)]
+    wave_crest = heapq.nlargest(wn, close_list, key=lambda x: x[1])
 
     # 计算最小的N个值，认为是波谷
-    wave_base = heapq.nsmallest(wn, enumerate(arr), key=lambda x: x[1])
+    # <class 'list'>: [(1, 3660.4), (0, 3677.0), (2, 3677.5), (3, 3677.8), (11, 3760.8)]
+    wave_base = heapq.nsmallest(wn, close_list, key=lambda x: x[1])
 
-    wave_crest_x = []  # 波峰x
-    wave_crest_y = []  # 波峰y
-    for i, j in wave_crest:
-        wave_crest_x.append(i)
-        wave_crest_y.append(j)
-
-    wave_base_x = []  # 波谷x
-    wave_base_y = []  # 波谷y
-    for i, j in wave_base:
-        wave_base_x.append(i)
-        wave_base_y.append(j)
-
-    return wave_crest_x, wave_base_x
+    return wave_crest, wave_base
 
 
 # 波峰波谷猜想
-wn = 10
-hs = -60
+wn = 5
+hs = -7 * 24
 
 
 def analysis(kline):
@@ -56,7 +50,7 @@ def analysis(kline):
     df['wave'] = 0
     df = df[hs:]
     data_len_ = len(df)
-    wave_crest_x, wave_base_x = wave_guess(df['close'].values, wn)
+    wave_crest_x, wave_base_x = wave_guess(df.values, wn)
     wave_base_x_ = []
     wave_crest_x_ = []
     for x in wave_crest_x:
@@ -95,9 +89,8 @@ def analysis(kline):
     return df.iloc[-1]
 
 
-filename = 'BitMEX-170901-190606-1H'
+filename = 'BitMEX-170901-191107-1H'
 df = data2df.csv2df(filename + '.csv')
-df = df.astype(float)
 df = df.astype(float)
 
 datas = df.values
@@ -112,11 +105,7 @@ for i in range(750, len(df)):
     test_df = datas[i - 750: i]
     row = analysis(test_df)
 
-    if row['signal'] in ['long', 'short'] and backtest.side == 'wait':
-        amount = int(backtest.asset * row['close'] * level)
-        backtest.create_order(row['signal'], "market", row['close'], amount)
-        # backtest.stop_price = row['%s_stop_price' % row['signal']]
-    elif row['signal'] in ['long', 'short'] and backtest.side != 'wait' and backtest.side != row['signal']:
+    if row['signal'] in ['long', 'short']:
         amount = int(backtest.asset * row['close'] * level)
         backtest.create_order(row['signal'], "market", row['close'], amount)
 
@@ -125,9 +114,8 @@ for i in range(750, len(df)):
         backtest.close_positions(row['close'], 'market')
 
     else:
-        backtest.add_data(row['close'], row['high'], row['low'])
+        backtest.add_data(row['close'])
 
-    print(i, row['close'], row['signal'], backtest.open_price, backtest.side, backtest.asset, backtest.float_profit,
-          backtest.asset + backtest.float_profit, row['wave'])
+    print(i, row['close'], row['signal'], backtest.asset, backtest.float_profit, backtest.asset + backtest.float_profit)
 
 backtest.show("Wave guess wn:%d, hs:%d" % (wn, hs))
