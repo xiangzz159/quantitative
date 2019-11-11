6d3557507a9dc8273bc2a999ee0e08bf67351689
xiangguangye <xiangguangye@sunline.com>
2019-11-11 17:35:58 +0800

    update wave strategies

--- test/WAVE_test.py
+++ test/WAVE_test.py
@@ -24,72 +24,66 @@ from tools.stockstats import StockDataFrame
 
 
 def wave_guess(arr, wn):
-    close_list = arr[:, [0, 4]]
-
+    # 取timestamp和close列
+    high_list = arr[:, [0, 2]]
+    low_list = arr[:, [0, 3]]
     # 计算最大的N个值，认为是波峰
-    # <class 'list'>: [(167, 4343.0), (166, 4335.0), (144, 4329.5), (146, 4329.3), (145, 4327.3)]
-    wave_crest = heapq.nlargest(wn, close_list, key=lambda x: x[1])
-
+    wave_crest = heapq.nlargest(wn, high_list, key=lambda x: x[1])
     # 计算最小的N个值，认为是波谷
-    # <class 'list'>: [(1, 3660.4), (0, 3677.0), (2, 3677.5), (3, 3677.8), (11, 3760.8)]
-    wave_base = heapq.nsmallest(wn, close_list, key=lambda x: x[1])
-
+    wave_base = heapq.nsmallest(wn, low_list, key=lambda x: x[1])
     return wave_crest, wave_base
 
 
-# 波峰波谷猜想
-wn = 5
-hs = -7 * 24
-
-
 def analysis(kline):
-    data_len = len(kline)
     df = pd.DataFrame(kline, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    stock = StockDataFrame.retype(df)
-
-    df['wave'] = 0
-    df = df[hs:]
-    data_len_ = len(df)
-    wave_crest_x, wave_base_x = wave_guess(df.values, wn)
-    wave_base_x_ = []
-    wave_crest_x_ = []
-    for x in wave_crest_x:
-        idx = data_len - data_len_ + x
-        wave_crest_x_.append(idx)
-    for x in wave_base_x:
-        idx = data_len - data_len_ + x
-        wave_base_x_.append(idx)
-    df.loc[wave_crest_x_, 'wave'] = 1
-    df.loc[wave_base_x_, 'wave'] = -1
-
     df['signal'] = 'wait'
-    # 策略：short
-    # df['signal'] = np.where((df['wave'] == 0) & (df['wave'].shift(1) == 1) & (df['wave'].shift(2) == 1), 'short',
-    #                         df['signal'])
-    df['signal'] = np.where((df['wave'] == 0) & (df['wave'].shift(1) == 0) & (df['wave'].shift(2) == 1), 'short',
-                            df['signal'])
-    df['signal'] = np.where(
-        (df['wave'] == 0) & (df['wave'].shift(1) == 0) & (df['wave'].shift(2) == 0) & (df['wave'].shift(3) == 1),
-        'short', df['signal'])
-    # 策略：long
-    # df['signal'] = np.where((df['wave'] == 0) & (df['wave'].shift(1) == -1) & (df['wave'].shift(2) == -1), 'long',
-    #                         df['signal'])
-    df['signal'] = np.where((df['wave'] == 0) & (df['wave'].shift(1) == 0) & (df['wave'].shift(2) == -1), 'long',
-                            df['signal'])
-    df['signal'] = np.where(
-        (df['wave'] == 0) & (df['wave'].shift(1) == 0) & (df['wave'].shift(2) == 0) & (df['wave'].shift(3) == -1),
-        'long', df['signal'])
 
-    # 平仓
-    df['signal'] = np.where((df['signal'] == 'wait') & (df['wave'] == -1) & (df['wave'].shift(1) == 0), 'close_long',
-                            df['signal'])
-    df['signal'] = np.where((df['signal'] == 'wait') & (df['wave'] == 1) & (df['wave'].shift(1) == 0), 'close_short',
-                            df['signal'])
+    # TODO 参数优化
+    wn = 10  # 取wn个最大最小值
+    hs = -300  # 截取的时间周期
+    stop_trade_times = 8  # 停止交易时间
+
+    df = df[hs:]
+    wave_crest, wave_base = wave_guess(df.values, wn)
+    df.index = df.timestamp
+    hts = []
+    lts = []
+    for x in wave_crest:
+        hts.append(x[0])
+    for x in wave_base:
+        lts.append(x[0])
+
+    mid_price = (wave_base[0][1] + wave_crest[0][1] + wave_base[-1][1] + wave_crest[-1][1]) / 4
+    # print(wave_base)
+    # print(wave_crest)
+
+    df.loc[[wave_crest[0][0]], 'signal'] = 'close_short'
+    df.loc[[wave_base[0][0]], 'signal'] = 'close_long'
+    for i in range(stop_trade_times):
+        df['signal'] = np.where((df['signal'] == 'wait') & (df['signal'].shift(1 + i) == 'close_short'), 'close_short',
+                                df['signal'])
+        df['signal'] = np.where((df['signal'] == 'wait') & (df['signal'].shift(1 + i) == 'close_long'), 'close_long',
+                                df['signal'])
+
+    df['signal'] = np.where(
+        (df['signal'] == 'wait') & (df['close'] > wave_crest[-1][1]) & (df['close'] < wave_crest[0][1]) & (
+                df['high'] < wave_crest[0][1]), 'short', df['signal'])
+
+    df['signal'] = np.where(
+        (df['signal'] == 'wait') & (df['close'] < wave_base[-1][1]) & (df['close'] > wave_base[0][1]) & (
+                df['low'] > wave_base[0][1]), 'long', df['signal'])
+
+    # df['signal'] = np.where((df['signal'] == 'wait') & (df['close'].shift(1) < mid_price) & (df['close'] >= mid_price),
+    #                         'close_short', df['signal'])
+    # df['signal'] = np.where((df['signal'] == 'wait') & (df['close'].shift(1) > mid_price) & (df['close'] <= mid_price),
+    #                         'close_long', df['signal'])
 
     return df.iloc[-1]
 
 
-filename = 'BitMEX-170901-191107-1H'
+# filename = 'BitMEX-170901-190606-5M'
+filename = 'BitMEX-170901-190606-15M'
+# filename = 'BitMEX-170901-190606-30M'
 df = data2df.csv2df(filename + '.csv')
 df = df.astype(float)
 
@@ -101,11 +95,16 @@ backtest = BmBackTest({
 
 level = 1
 
-for i in range(750, len(df)):
+# for i in range(750, len(df)):
+for i in range(750, 10000):
     test_df = datas[i - 750: i]
     row = analysis(test_df)
 
-    if row['signal'] in ['long', 'short']:
+    if row['signal'] in ['long', 'short'] and backtest.side == 'wait':
+        amount = int(backtest.asset * row['close'] * level)
+        backtest.create_order(row['signal'], "market", row['close'], amount)
+        # backtest.stop_price = row['%s_stop_price' % row['signal']]
+    elif row['signal'] in ['long', 'short'] and backtest.side != 'wait' and backtest.side != row['signal']:
         amount = int(backtest.asset * row['close'] * level)
         backtest.create_order(row['signal'], "market", row['close'], amount)
 
@@ -114,8 +113,9 @@ for i in range(750, len(df)):
         backtest.close_positions(row['close'], 'market')
 
     else:
-        backtest.add_data(row['close'])
+        backtest.add_data(row['close'], row['high'], row['low'])
 
-    print(i, row['close'], row['signal'], backtest.asset, backtest.float_profit, backtest.asset + backtest.float_profit)
+    print(i, row['close'], row['signal'], backtest.open_price, backtest.side, backtest.asset, backtest.float_profit,
+          backtest.asset + backtest.float_profit)
 
-backtest.show("Wave guess wn:%d, hs:%d" % (wn, hs))
+backtest.show("Wave guess")
